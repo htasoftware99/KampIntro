12358834390a018666b51c7cacfbb612f5a7435d
Hasan <hasan_tugra1999@outlook.com>
2022-01-30 15:02:23 +0300

    Collections

---
+++ Collections/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Ali", "Veli", "Hasan", "Tarık"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+            Console.WriteLine(names[3]);
+            names.Add("Alp");
+            Console.WriteLine(names[4]);
+            Console.WriteLine(names[0]);
+        }
+    }
+}
