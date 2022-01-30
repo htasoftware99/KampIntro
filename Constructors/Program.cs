6710bbd5c06aee219d27e65a1b6756206bec2fe7
Hasan <hasan_tugra1999@outlook.com>
2022-01-30 21:52:36 +0300

    Constructor

---
+++ Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Constructors 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() { Id= 1, Name= "Hasan", Surname="Aykaç", City = "Ankara"};
+            Customer customer2 = new Customer(2,"Bial", "Bilgili", "Çorum");
+            
+        }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        //Default Constructor
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            Name = firstName;
+            Surname = lastName;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+    }
+}
