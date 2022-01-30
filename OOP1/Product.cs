e17437392552985b6dbcbcce4af5367a6fbd9c3e
Hasan <hasan_tugra1999@outlook.com>
2022-01-30 14:02:48 +0300

    OOP1

---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Product
+    {
+        public int Id { get; set; }             // Primary Key
+        public int CategoryId { get; set; }     // Foreign key
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP1 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.CategoryId = 2; //Mobilya
+            product.ProductName = "Masa";
+            product.UnitPrice = 1000;
+            product.UnitsInStock = 50;
+
+            Product product2 = new Product() { Id = 2, CategoryId = 3,
+                ProductName = "Süt", UnitPrice = 15, UnitsInStock = 25 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+            
+        }
+    }
+}
